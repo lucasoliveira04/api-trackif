a9e58ffe16ef2fddb622a87b52ff3ec60e0ed857
Lucas <camposdlucasoli@gmail.com>
2024-08-10 13:03:29 -0300

    init

---
+++ src/main/java/org/trackifapi/TrackifApiApplication.java
@@ -0,0 +1,13 @@
+package org.trackifapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackifApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackifApiApplication.class, args);
+    }
+
+}
