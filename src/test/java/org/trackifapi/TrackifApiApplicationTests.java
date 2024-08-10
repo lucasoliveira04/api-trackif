3aeef3100a9d22136b9272b131e3bd563d88b315
Lucas <camposdlucasoli@gmail.com>
2024-08-10 13:03:34 -0300

    init

---
+++ src/test/java/org/trackifapi/TrackifApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.trackifapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackifApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
