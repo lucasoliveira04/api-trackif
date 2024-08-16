6143e2fde2e22330acf14b2e708dbf5674822116
Lucas <camposdlucasoli@gmail.com>
2024-08-16 19:58:16 -0300

    Extendendo JpaRepository nas tabela

--- src/main/java/org/trackifapi/modal/repository/UserChildRepository.java
+++ src/main/java/org/trackifapi/modal/repository/UserChildRepository.java
@@ -1,7 +1,9 @@
 package org.trackifapi.modal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.trackifapi.modal.entity.UserChild;
 
+@Repository
 public interface UserChildRepository extends JpaRepository<UserChild, Integer> {
 }
