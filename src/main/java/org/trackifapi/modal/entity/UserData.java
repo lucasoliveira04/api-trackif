3272dc272bfd29b8763c7b8a83af0217915391ca
Lucas <camposdlucasoli@gmail.com>
2024-08-16 19:55:18 -0300

    Tabela de usuarios principal

---
+++ src/main/java/org/trackifapi/modal/entity/UserData.java
@@ -0,0 +1,39 @@
+package org.trackifapi.modal.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.trackifapi.enums.StatusEnum;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@Table(name = "users")
+public class UserData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "cpf")
+    private String cpf;
+
+    @Column(name = "rg")
+    private String rg;
+
+    @Column(name = "role")
+    private String role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private StatusEnum status;
+
+    @Column(name = "createdAt")
+    private LocalDateTime createdAt;
+}
