5113f624be28aaf890ccc474d76c12b6a984730a
Lucas <camposdlucasoli@gmail.com>
2024-08-17 17:09:10 -0300

    dto de user

--- src/main/java/org/trackifapi/modal/dto/UserDataDto.java
+++ src/main/java/org/trackifapi/modal/dto/UserDataDto.java
@@ -1,30 +1,44 @@
 package org.trackifapi.modal.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 import org.trackifapi.enums.StatusEnum;
 import org.trackifapi.modal.entity.UserData;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public record UserDataDto(
-        Integer id,
-        String name,
-        String email,
-        String cpf,
-        String rg,
-        String role,
-        StatusEnum status,
-        LocalDateTime createdAt
-) {
-    public static UserDataDto toDTO(UserData userData) {
-        return new UserDataDto(
-                userData.getId(),
-                userData.getName(),
-                userData.getEmail(),
-                userData.getCpf(),
-                userData.getRg(),
-                userData.getRole(),
-                userData.getStatus(),
-                userData.getCreatedAt()
-        );
+@Builder
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserDataDto implements Serializable {
+    private Integer id;
+    private String name;
+    private String email;
+    private String telephone;
+    private String cpf;
+    private String rg;
+    private String role;
+    private  StatusEnum status;
+    private LocalDateTime createdAt;
+
+    public static UserDataDto fromEntity(UserData userData){
+        return UserDataDto.builder()
+                .id(userData.getId())
+                .name(userData.getName())
+                .email(userData.getEmail())
+                .telephone(userData.getTelephone())
+                .cpf(userData.getCpf())
+                .rg(userData.getRg())
+                .role(userData.getRole())
+                .status(userData.getStatus())
+                .createdAt(userData.getCreatedAt())
+                .build();
     }
+
 }
