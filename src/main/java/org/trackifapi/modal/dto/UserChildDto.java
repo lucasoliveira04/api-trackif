4ddc8641307bae7eb5c680054942284a5667385d
Lucas <camposdlucasoli@gmail.com>
2024-08-17 17:09:03 -0300

    dto de user child

--- src/main/java/org/trackifapi/modal/dto/UserChildDto.java
+++ src/main/java/org/trackifapi/modal/dto/UserChildDto.java
@@ -1,17 +1,29 @@
 package org.trackifapi.modal.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Value;
 import org.trackifapi.modal.entity.UserChild;
+import org.trackifapi.modal.entity.UserData;
 
-public record UserChildDto(
-        Integer id,
-        String token,
-        UserDataDto userData
-) {
-    public static UserChildDto toDTO(UserChild userChild) {
-        return new UserChildDto(
-                userChild.getId(),
-                userChild.getToken(),
-                UserDataDto.toDTO(userChild.getUserData())
-        );
+import java.io.Serializable;
+
+@Builder
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserChildDto implements Serializable {
+    private Integer id;
+    private UserDataDto userData;
+
+    public static UserChildDto fromEntity(UserChild userChild){
+        return UserChildDto.builder()
+                .id(userChild.getId())
+                .userData(UserDataDto.fromEntity(userChild.getUserData()))
+                .build();
     }
 }
