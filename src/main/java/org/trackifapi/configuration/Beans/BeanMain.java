5851e42164f8a416f21896d110e2b519732f644f
Lucas <camposdlucasoli@gmail.com>
2024-08-17 17:07:22 -0300

    beans

--- src/main/java/org/trackifapi/configuration/Beans/BeanMain.java
+++ src/main/java/org/trackifapi/configuration/Beans/BeanMain.java
@@ -1,5 +1,6 @@
 package org.trackifapi.configuration.Beans;
 
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
