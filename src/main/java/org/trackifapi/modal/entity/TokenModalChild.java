c0f349eb9a4f5f763e7214bfbd7862ec49dd1d76
Lucas <camposdlucasoli@gmail.com>
2024-08-17 17:08:27 -0300

    Tabela de tokens

---
+++ src/main/java/org/trackifapi/modal/entity/TokenModalChild.java
@@ -0,0 +1,29 @@
+package org.trackifapi.modal.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "token_child")
+@Getter @Setter
+public class TokenModalChild {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "token")
+    private String token;
+
+    @Column(name = "createdAt")
+    private LocalDateTime createdAt;
+
+    @Column(name = "expiresAt")
+    private LocalDateTime expiresAt;
+
+    @OneToOne
+    @JoinColumn(name = "user_child_id", referencedColumnName = "id")
+    private UserChild userChild;
+}
