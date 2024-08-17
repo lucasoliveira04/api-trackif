f0a4e425f309f862361847854a1bc77713667a4e
Lucas <camposdlucasoli@gmail.com>
2024-08-17 20:50:56 -0300

    controller de localização

--- src/main/java/org/trackifapi/controller/Location/GeocodingController.java
+++ src/main/java/org/trackifapi/controller/Location/GeocodingController.java
@@ -1,17 +1,20 @@
-package org.trackifapi.controller;
+package org.trackifapi.controller.Location;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.trackifapi.services.GenerateToken.GenerateTokenServices;
 import org.trackifapi.services.GeoLocation.GeocodingService;
 
 @RestController
 public class GeocodingController {
 
     private final GeocodingService geocodingService;
+    private final GenerateTokenServices generateTokenServices;
 
-    public GeocodingController(GeocodingService geocodingService) {
+    public GeocodingController(GeocodingService geocodingService, GenerateTokenServices generateTokenServices) {
         this.geocodingService = geocodingService;
+        this.generateTokenServices = generateTokenServices;
     }
 
     @GetMapping("/get-address")
