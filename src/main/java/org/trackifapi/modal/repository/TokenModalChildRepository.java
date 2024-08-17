6dc52263799390c71561b8621be68f9bef7e6b22
Lucas <camposdlucasoli@gmail.com>
2024-08-17 17:08:37 -0300

    repositorio de tokens

---
+++ src/main/java/org/trackifapi/modal/repository/TokenModalChildRepository.java
@@ -0,0 +1,7 @@
+package org.trackifapi.modal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.trackifapi.modal.entity.TokenModalChild;
+
+public interface TokenModalChildRepository extends JpaRepository<TokenModalChild, Integer> {
+}
