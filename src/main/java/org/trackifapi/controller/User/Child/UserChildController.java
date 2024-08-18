403eb73bcb619ff6ae97cf4e1e410734d0af6d33
Lucas <camposdlucasoli@gmail.com>
2024-08-18 10:47:57 -0300

    controlador do user child

---
+++ src/main/java/org/trackifapi/controller/User/Child/UserChildController.java
@@ -0,0 +1,37 @@
+package org.trackifapi.controller.User.Child;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.trackifapi.modal.dto.TokenModalChildDto;
+import org.trackifapi.modal.dto.UserChildDto;
+import org.trackifapi.services.User.Child.AdduserChild;
+import org.trackifapi.services.User.Child.Token.AddToken;
+
+@RestController
+@RequestMapping("/api/user-child")
+public class UserChildController {
+    private final AdduserChild adduserChild;
+    private final AddToken addToken;
+
+    public UserChildController(AdduserChild adduserChild, AddToken addToken) {
+        this.adduserChild = adduserChild;
+        this.addToken = addToken;
+    }
+
+    // adicionando token do user child
+    @PostMapping("/add-token")
+    public ResponseEntity<?> addTokenUserChild(
+            @RequestBody TokenModalChildDto tokenModalChildDto,
+            @RequestParam double lat,
+            @RequestParam double lng)
+    {
+        return addToken.addToken(tokenModalChildDto, lat, lng);
+    }
+
+
+    // Adicionando user child
+    @PostMapping("/add/user-child")
+    public ResponseEntity<?> addUserChild(@RequestBody UserChildDto userChildDto){
+        return adduserChild.addUserChild(userChildDto);
+    }
+}
