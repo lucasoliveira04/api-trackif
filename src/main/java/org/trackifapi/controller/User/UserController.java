807fc531a985849121e9bc8f07a74512ed314591
Lucas <camposdlucasoli@gmail.com>
2024-08-18 10:48:04 -0300

    controlador do user

--- src/main/java/org/trackifapi/controller/User/UserController.java
+++ src/main/java/org/trackifapi/controller/User/UserController.java
@@ -5,20 +5,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.trackifapi.modal.dto.UserChildDto;
 import org.trackifapi.modal.dto.UserDataDto;
-import org.trackifapi.services.User.Child.AdduserChild;
 import org.trackifapi.services.User.User.AddUser;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
     private final AddUser addUser;
-    private final AdduserChild adduserChild;
 
-    public UserController(AddUser addUser, AdduserChild adduserChild) {
+    public UserController(AddUser addUser) {
         this.addUser = addUser;
-        this.adduserChild = adduserChild;
     }
 
     @PostMapping("/add/user")
@@ -26,8 +22,5 @@ public class UserController {
         return addUser.addUser(userDataDto);
     }
 
-    @PostMapping("/add/user-child")
-    public ResponseEntity<?> addUserChild(@RequestBody UserChildDto userChildDto){
-        return adduserChild.addUserChild(userChildDto);
-    }
+
 }
