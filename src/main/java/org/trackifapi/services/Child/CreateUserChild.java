cf9c3d5345bea976f4ee2d4fc8c5116fbffbfe11
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-09-26 15:59:08 -0300

    feat : obtendo endereço com o cep

--- src/main/java/org/trackifapi/services/Child/CreateUserChild.java
+++ src/main/java/org/trackifapi/services/Child/CreateUserChild.java
@@ -1,5 +1,6 @@
 package org.trackifapi.services.Child;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -9,7 +10,10 @@ import org.trackifapi.modal.dto.UsuarioChildDto;
 import org.trackifapi.modal.entity.Child.UsuarioChild;
 import org.trackifapi.modal.repository.TokenChildRepository;
 import org.trackifapi.modal.repository.UsuarioChildRepository;
+import org.trackifapi.util.ApiAddressCep;
 import org.trackifapi.util.CheckUserRole;
+
+import java.util.Map;
 
 @Service
 public class CreateUserChild {
@@ -17,27 +21,43 @@ public class CreateUserChild {
     private final UsuarioChildRepository usuarioChildRepository;
     private final TokenChildRepository tokenChildRepository;
     private final CheckUserRole checkUserRole;
+    private final ApiAddressCep apiAddressCep;
 
-    public CreateUserChild(UsuarioChildRepository usuarioChildRepository, TokenChildRepository tokenChildRepository, CheckUserRole checkUserRole) {
+    public CreateUserChild(UsuarioChildRepository usuarioChildRepository, TokenChildRepository tokenChildRepository, CheckUserRole checkUserRole, ApiAddressCep apiAddressCep) {
         this.usuarioChildRepository = usuarioChildRepository;
         this.tokenChildRepository = tokenChildRepository;
         this.checkUserRole = checkUserRole;
+        this.apiAddressCep = apiAddressCep;
     }
 
     public ResponseEntity<?> createUsuarioChild(UsuarioChildDto dto) {
         try{
             checkUserRole.checkToUser();
             RolesEnum roles = checkUserRole.getRole();
+
+            String cep = dto.getEndereco().getCep();
+            if (cep == null || cep.trim().isEmpty()){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("CEP não pode ser vazio");
+            }
+
+            String cepInfo = apiAddressCep.getAddressByCep(dto.getEndereco().getCep());
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, String> enderecoMap = mapper.readValue(cepInfo, Map.class);
+
+            if (enderecoMap.containsKey("erro")){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("CEP inválido");
+            }
+
             UsuarioChild usuarioChild = new UsuarioChild(
                     dto.getNome(),
                     dto.getEmail(),
                     dto.getTelefone(),
                     dto.getCpf(),
                     dto.getRg(),
-                    dto.getEndereco().getRua(),
-                    dto.getEndereco().getEstado(),
-                    dto.getEndereco().getCidade(),
-                    dto.getEndereco().getBairro(),
+                    enderecoMap.get("logradouro"),
+                    enderecoMap.get("uf"),
+                    enderecoMap.get("localidade"),
+                    enderecoMap.get("bairro"),
                     dto.getEndereco().getCep(),
                     roles
             );
