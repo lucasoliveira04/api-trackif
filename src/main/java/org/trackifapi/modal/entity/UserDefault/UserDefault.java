3d992e6f07ecd5757b76e6b0bc0c691f84e62e6a
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-09-27 13:45:03 -0300

    feat : tabela de usuario default e dto criadas

---
+++ src/main/java/org/trackifapi/modal/entity/UserDefault/UserDefault.java
@@ -0,0 +1,30 @@
+package org.trackifapi.modal.entity.UserDefault;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Getter;
+import lombok.Setter;
+import org.trackifapi.Enums.RolesEnum;
+import org.trackifapi.modal.entity.UsuarioBase;
+
+@Entity
+@Getter @Setter
+public class UserDefault extends UsuarioBase {
+
+        @Enumerated(EnumType.STRING)
+        @Column(name = "roles")
+        private RolesEnum roles;
+
+        public UserDefault() {
+            super();
+        }
+
+        public UserDefault(String nome, String email, String telefone, String cpf, String rg,
+                        String rua, String estado, String cidade, String bairro, String cep, RolesEnum roles) {
+            super(nome, email, telefone, cpf, rg, rua, bairro, cidade, estado, cep);
+
+            this.roles = roles;
+        }
+}
