0b7fb9f507347ef724459424ec052c23ee8d36b4
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-10-06 19:56:21 -0300

    refactor : usando componente que converte o dto em uma entity

--- src/main/java/org/trackifapi/services/AddedUser/AddedUserFather.java
+++ src/main/java/org/trackifapi/services/AddedUser/AddedUserFather.java
@@ -1,8 +1,7 @@
 package org.trackifapi.services.AddedUser;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.trackifapi.Enum.RoleEnum;
+import org.trackifapi.mapper.UserMapper;
 import org.trackifapi.modal.Repository.UserFatherModalRepository;
 import org.trackifapi.modal.dto.UserDto;
 import org.trackifapi.modal.entity.father.UserFatherModal;
@@ -11,21 +10,18 @@ import org.trackifapi.services.interfaces.IUserService;
 @Service
 public class AddedUserFather implements IUserService {
 
-    @Autowired
-    private UserFatherModalRepository userFatherModalRepository;
+    private final UserFatherModalRepository userFatherModalRepository;
+    private final UserMapper userMapper;
+
+    public AddedUserFather(UserFatherModalRepository userFatherModalRepository, UserMapper userMapper) {
+        this.userFatherModalRepository = userFatherModalRepository;
+        this.userMapper = userMapper;
+    }
 
     @Override
     public void addUser(UserDto user) {
         UserFatherModal userFatherModal = new UserFatherModal();
-        userFatherModal.setName(user.getName());
-        userFatherModal.setEmail(user.getEmail());
-        userFatherModal.setPhone(user.getPhone());
-        userFatherModal.setRg(user.getRg());
-        userFatherModal.setCpf(user.getCpf());
-        userFatherModal.setAge(user.getAge());
-        userFatherModal.setDateBirth(user.getDateBirth());
-        userFatherModal.setGender(user.getGender());
-        userFatherModal.setRoleEnum(RoleEnum.USER_FATHER);
+        UserMapper.mapDtoToUser(user, userFatherModal);
         userFatherModalRepository.save(userFatherModal);
     }
 }
