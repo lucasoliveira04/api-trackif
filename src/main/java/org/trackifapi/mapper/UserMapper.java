d06148b88e57396c4d5beb88c8b31b49a201d152
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-10-08 10:08:28 -0300

    feat : aplicando regex

--- src/main/java/org/trackifapi/mapper/UserMapper.java
+++ src/main/java/org/trackifapi/mapper/UserMapper.java
@@ -3,12 +3,22 @@ package org.trackifapi.mapper;
 import org.springframework.stereotype.Component;
 import org.trackifapi.modal.dto.UserDto;
 import org.trackifapi.modal.interfaces.IUser;
+import org.trackifapi.regex.ApplyRegex;
+import org.trackifapi.regex.HashMapRegex;
 
 /**
  * Mapper que mapeia um DTO para um usuário.
  */
 @Component
 public class UserMapper {
+    private final HashMapRegex hashMapRegex;
+    private final ApplyRegex applyRegex;
+
+    public UserMapper(HashMapRegex hashMapRegex, ApplyRegex applyRegex) {
+        this.hashMapRegex = hashMapRegex;
+        this.applyRegex = applyRegex;
+    }
+
     /**
      * Mapeia um DTO de usuário para uma entidade de usuário.
      *
@@ -16,7 +26,7 @@ public class UserMapper {
      * @param userDto O DTO do usuário a ser mapeado
      * @param entity  A entidade de usuário a ser preenchida com os dados do DTO
      */
-    public static <T extends IUser> T mapDtoToUser(UserDto userDto, T entity){
+    public static <T extends IUser> T mapDtoToUser(UserDto userDto, T entity) {
         entity.setName(userDto.getName());
         entity.setEmail(userDto.getEmail());
         entity.setPhone(userDto.getPhone());
@@ -27,4 +37,11 @@ public class UserMapper {
         entity.setGender(userDto.getGender());
         return entity;
     }
+
+    public void applyRegex(UserDto userDto) {
+        ApplyRegex<UserDto> regexValidator = new ApplyRegex<>(userDto, hashMapRegex);
+        regexValidator.applyRegex();
+    }
+
 }
+
